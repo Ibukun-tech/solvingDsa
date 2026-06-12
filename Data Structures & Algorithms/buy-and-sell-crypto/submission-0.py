a385195c95540caf766e1c29efa57ca4ee5f5cc7
Oyetunji Ibukunoluwa Oluwole <78256143+Ibukun-tech@users.noreply.github.com>
2026-06-12 12:28:39 +0100

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r= 0, 1
+        maxProfe=0
+        while r < len(prices):
+            if prices[r]>prices[l]:
+                maxProfe= max(maxProfe, prices[r]-prices[l])
+            else:
+                l=r
+            r+=1
+        return maxProfe    
